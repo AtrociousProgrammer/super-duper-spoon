cc2517e3c653572f0a89bf203db5bde74d362e98
AtrociousProgrammer <atrociousprogrammer@github.com>
2022-11-29 15:43:13 +0530

    refactor: render from jinja template(with dummy user)

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,23 @@
 
+from flask import render_template
 from app import flask_app
 
 @flask_app.route('/')
 @flask_app.route('/index')
 def index():
-    return '''Hello, World!'''
+    user = {'username': 'Emanuel'}
+    posts = [
+            {
+                'author': 'Juan',
+                'body': 'Fuck You'
+
+                },
+            {
+                'author': 'Jose',
+                'body': 'Fucking retard'
+                },
+            ]
+            
+    return render_template('index.html', title='Welcome!', user=user,
+            posts=posts)
+

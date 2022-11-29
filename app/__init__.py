e10580232e1182be96256ddfc4fe8af294679666
AtrociousProgrammer <atrociousprogrammer@github.com>
2022-11-29 15:40:33 +0530

    refactor: configured flask_app and create a instance/ dir

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,20 @@
+import os
+
 from flask import Flask
 
-flask_app = Flask(__name__)
+flask_app = Flask(__name__, instance_relative_config=True)
 
+flask_app.config.from_mapping(
+        SECRET_KEY = 'dev',
+        DATABASE = os.path.join(flask_app.instance_path, 'super_duper_spoon.sqlite')
+        )
+
+# create instance/ dir which contains files not to be commited
+# like sqlite db
+if not os.path.exists(flask_app.instance_path):
+
+    os.makedirs(flask_app.instance_path)
+
+# declared down here to avoid circular imports
+# app/routes.py includes `from app import flask_app`
 from app import routes
